548a22e70b04f86d1d6ce3c3ca49be2b32250ba6
Sleepy Cat <gilb2966@gmail.com>
2023-04-04 17:51:23 -0500

    added css styling

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,20 @@
 let startBtn = document.querySelector("#start");
+let clickBox = document.querySelector(".container");
+
+
+let quizTimer = 0;
 
 
 
 
 
+function startTimer () {
 
+}
 
+function showQuestion () {
 
-
+}
 
 
 
@@ -18,7 +25,7 @@ let startBtn = document.querySelector("#start");
 // - user arrives at the website and sees a central box with a start button below, a timer and high scores somewhere on the page -> create necessary html elements w/ classes or ids to access using js
 // - user starts the quiz by clicking the start button -> eventlistener
 // - timer starts -> function
-// - multiple choice question is presented to user -> function
+// - multiple choice question is presented to user -> function/ create/modify/append
 // - user clicks on a choice -> eventlistener
 // - evaluates whether choice is right or wrong -> function
 // - display right or wrong -> function 
@@ -29,8 +36,5 @@ let startBtn = document.querySelector("#start");
 
 
 
+clickBox.addEventListener("click", function())
 
-
-
-
-startBtn.addEventListener("click", function())
