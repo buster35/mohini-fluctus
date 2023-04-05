120180690a8357ad08169ba4385551c1aad5cb2c
buster35 <gilb2966@gmail.com>
2023-04-04 22:55:34 -0500

    built quiz questions in object variables

--- Assets/script.js
+++ Assets/script.js
@@ -2,6 +2,46 @@ let startBtn = document.querySelector("#start");
 let clickBox = document.querySelector(".container");
 let timeCount = document.querySelector("#time");
 
+//Quiz Questions//
+let question1 = {
+  question: "In the popular 90's sitcom Seinfeld, Jerry Seinfeld had a neighbor across the hall whom the referred to as Kramer. What is this character's first name?",
+  answer1: "Charles",
+  answer2: "Robert",
+  answer3: "Kosmo",
+  answer4: "Kermit",
+};
+
+let question2 = {
+  question: "How many species of turtles are there?",
+  answer1: "over 500",
+  answer2: "over 310",
+  answer3: "over 50",
+  answer4: "over 10",
+}
+
+let question3 = {
+  question: "What is the Dharma Initiative?",
+  answer1: "a non-profit organization",
+  answer2: "a research project on a mysterious island focused on fringe science",
+  answer3: "the latest trending cocktail",
+  answer4: "nothing; it hasn't been created yet",
+}
+
+let question4 = {
+  question: "In the ages-old game of chess, what's the only move where a player can move more than one piece?",
+  answer1: "the Check",
+  answer2: "the Pin",
+  answer3: "the Checkmate",
+  answer4: "the Castle",
+}
+
+let question5 = {
+  question: "What is the best kind of coffee?",
+  answer1: "Americano",
+  answer2: "regular drip",
+  answer3: "French press",
+  answer4: "Trick question; any method of ingesting coffee is the best!",
+}
 
 let quizTimer;
 
@@ -19,7 +59,7 @@ function startTimer () { //we want to start a countdown timer
 
 function showQuestion () {
   let newElement = document.getElementById("box1");
-  newElement.textContent = "In the popular 90's sitcom Seinfeld, Jerry Seinfeld had a neighbor across the hall whom the referred to as Kramer. What is this character's first name?"
+  newElement.textContent = 
 }
 
 function evalAnswer () {
